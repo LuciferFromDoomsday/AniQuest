8813127cb4a9a98b5aa5059ab732339c2994cbb0
LuciferFromDoomsday <48617655+LuciferFromDoomsday@users.noreply.github.com>
2021-01-08 19:25:21 +0600

    Initial Commit

---
+++ AniQuest/AniQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniQuestApp.swift
+//  AniQuest
+//
+//  Created by admin on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AniQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
